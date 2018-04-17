f6538a7013daa2eb1f2ef8710e4cc31d35cf2ba7
Novil <novil95@yandex.ru>
2018-04-17 10:57:49 +0500

    New

--- Drag.js
+++ Drag.js
@@ -1,10 +1,1 @@
-$("#draggable").draggable();
-#draggable {
-  width: 100px;
-  height: 100px;
-  position: absolute;
-  left: 50px;
-  top: 50px;
-  border-radius: 50%;
-  background: blue;
-}
+$("Map.png").draggable();
